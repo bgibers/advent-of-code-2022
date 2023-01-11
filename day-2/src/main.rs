e6b4ff64662b88a04ec3007728efcce345f6a8e4
Brendan Giberson <brendan.giberson@blackbaud.me>
2023-01-11 14:59:51 -0500

    Day 2B complete

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -2,15 +2,15 @@ use std::collections::HashMap;
 
 fn main() {
     let score = HashMap::from([
-        ("A X" , 4),
-        ("A Y" , 8),
-        ("A Z" , 3),
-        ("B X" , 1),
-        ("B Y" , 5),
-        ("B Z" , 9),
-        ("C X" , 7),
-        ("C Y" , 2),
-        ("C Z" , 6),
+        ("A X" , 0 + 3),
+        ("A Y" , 3 + 1),
+        ("A Z" , 6 + 2),
+        ("B X" , 0 + 1),
+        ("B Y" , 3 + 2),
+        ("B Z" , 6 + 3),
+        ("C X" , 0 + 2),
+        ("C Y" , 3 + 3),
+        ("C Z" , 6 + 1),
     ]);
 
     let input = include_str!("./input.txt");
