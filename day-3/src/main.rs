39c9413ea330e01168e12405b43be1f0acdb7f21
Brendan Giberson <brendan.giberson@blackbaud.me>
2023-01-18 11:25:09 -0500

    Day 3 pt 2

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,9 +1,9 @@
 use std::collections::HashSet;
 
 
-fn main() {
+fn pt1() {
     let mut sum = 0;
-    let sacks = include_str!("./input.txt").lines().for_each(|line|
+    include_str!("./input.txt").lines().for_each(|line|
     {
         let cmpt_size = line.chars().count() / 2;
 
@@ -22,4 +22,42 @@ fn main() {
     });
 
     println!("{}", sum);
+}
+
+fn pt2() {
+    let mut sacks = include_str!("./input.txt").lines();
+    let mut sum = 0;
+    
+    loop {
+        let line1 = sacks.next().unwrap_or("_");
+
+        if line1 == "_" {
+            break;
+        }
+
+        let line2 = sacks.next().unwrap();
+        let line3 = sacks.next().unwrap();
+
+        let first_set: HashSet<char> = line1.chars().collect();
+        let second_set: HashSet<char> = line2.chars().collect();
+        let third_set: HashSet<char> = line3.chars().collect();
+
+        let common_1: HashSet<char>  = first_set.intersection(&second_set).cloned().collect();
+        let common = common_1.intersection(&third_set).nth(0).unwrap();
+
+        let value = match common {
+            'a'..='z' => *common as u32 - 'a' as u32,
+            'A'..='Z' => *common as u32 - 'A' as u32 + 26,
+            _ => panic!("{common}"),
+        };
+
+        sum += value + 1;
+    }
+
+    println!("{}", sum);
+}
+
+fn main() {
+    pt1();
+    pt2();
 }
